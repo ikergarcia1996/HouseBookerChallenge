b87696327a87368b3ede3eeda31d3b782a260f83
Iker García Ferrero <Iker@Iker>
2016-05-06 12:35:43 +0200

    Restaurado el trabajar sobre el directorio por defecto

--- HouseBookerChallenge/src/CloudUtilities/FileManager.java
+++ HouseBookerChallenge/src/CloudUtilities/FileManager.java
@@ -1,54 +1,26 @@
 package CloudUtilities;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
+ 
 public class FileManager {
 
-	//Para borrar del directorio donde se esta ejecutand la app:
-	//fileRoute.append(System.getProperty("user.dir"));
 	
-	
-	// fl = nombre del fichero a borrar (se borra de la ruta donde esta
-	// ejecutandose
-	// El programa).
-	
-	public void removeFile(String route) {
+	//fl = nombre del fichero a borrar (se borra de la ruta donde esta ejecutandose
+	//El programa). 
+	public void removeFile (String fl){
+	StringBuilder fileRoute = new StringBuilder();  
+	fileRoute.append(System.getProperty("user.dir"));
+	fileRoute.append("\\");
+	fileRoute.append(fl);
+	System.out.println("Borrando el fichero " + fileRoute.toString());
+	try {
+		Files.delete(Paths.get(fileRoute.toString()));
+	} catch (IOException e) {
+		System.out.println("Error al borrar el archivo");
+		e.printStackTrace();
 		
-		System.out.println("Borrando el fichero " + route);
-		try {
-			Files.delete(Paths.get(route));
-		} catch (IOException e) {
-			System.out.println("Error al borrar el archivo");
-			e.printStackTrace();
-
-		}
 	}
-	
-	public String GetTempRoute(){
-		StringBuilder str = new StringBuilder();		
-				Process p = null;
-		try {
-			p = Runtime.getRuntime().exec("cmd.exe /c echo %TMP%");
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} 
-		BufferedReader br = new BufferedReader 
-		( new InputStreamReader( p.getInputStream() ) ); 
-		String tmp = null;
-		try {
-			tmp = br.readLine();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		str.append(tmp);
-		str.append("\\HBC\\");
-	return str.toString();
 	}
-	
 }
