e10286bd97006c66caedb21487f64d185c786393
kibah <kibah@Shadowknight>
2016-05-12 19:14:20 +0200

    Ooooootro parche

--- HouseBookerChallenge/src/dataAccess/DataAccess.java
+++ HouseBookerChallenge/src/dataAccess/DataAccess.java
@@ -645,19 +645,12 @@ public class DataAccess {
 	public void rhDelete(RuralHouse rh) {
 		// TODO Auto-generated method stub
 		ObjectSet<Object> res = db.queryByExample(rh);
-		ObjectSet<Object> res2;
 		if (!res.isEmpty()) {
 			RuralHouse rh1 = (RuralHouse) res.next();
 			for (Offer o: rh1.getOffers()) {
-				res2 = db.queryByExample(o);
-				db.delete(res2.next());
+				db.delete(db.queryByExample(o));
 			}
-			res2 = db.queryByExample(rh1.getOwner());
-			Propietario p = (Propietario) res2.next();
-			p.getCasas().remove(rh1);
-			db.delete(rh1);
-			db.store(p);
-			db.commit();
+			db.delete(db.queryByExample(rh1));
 		}
 	}
 	
@@ -668,11 +661,7 @@ public class DataAccess {
 			Offer o1 = (Offer) res.next();
 			ObjectSet<Object> res2 = db.queryByExample(o1.getRuralHouse());
 			if (!res2.isEmpty()) {
-				RuralHouse rh = (RuralHouse) res2.next();
-				rh.getOffers().remove(o1);
-				db.delete(o1);
-				db.store(rh);
-				db.commit();
+				db.delete(db.queryByExample(o1));
 			}
 		}
 	}
